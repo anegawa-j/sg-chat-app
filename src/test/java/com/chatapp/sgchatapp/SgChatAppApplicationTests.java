e0a00b73af89a0ea6792b7fa60e607368015a81d
姉川純一朗 <anegawa_j@anegawajunichirounoMacBook-Pro.local>
2019-12-21 18:06:08 +0900

    init project

---
+++ src/test/java/com/chatapp/sgchatapp/SgChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.sgchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
